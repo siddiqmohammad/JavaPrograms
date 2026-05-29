23bef76cee26c2b99786fb544223964a933e7845
mohammad siddiq <siddimohammad770@gmail.com>
2026-05-29 21:22:48 +0530

    feat:PROFIT SHARE

--- src/com/codegnan/fundamentals/Profitshare.java
+++ src/com/codegnan/fundamentals/Profitshare.java
@@ -1,10 +1,30 @@
 package com.codegnan.fundamentals;
 
+import java.util.Scanner;
+
 public class Profitshare {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		int investmentA=sc.nextInt();
+		int monthsA =sc.nextInt();
+		int investmentB=sc.nextInt();
+		int monthsB =sc.nextInt();
+		int joinedAterMonthsofA=sc.nextInt();
+		int effectiveA=investmentA*monthsA;
+		int monthsB=12-joinedAterMonthsofA;
+		int effectiveB=investmentB*monthsB;
+		int totalRatio=effectiveA+effectiveB;
+		double bshare=((double)effectiveB/totalratio))*profit;
+		System.out.println("b share:",+bShare);
+		sc.close();
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
