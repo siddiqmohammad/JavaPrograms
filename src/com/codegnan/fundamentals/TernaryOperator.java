755d4da18c32d72a3933bae5cb45edffcbe3f20d
mohammad siddiq <siddimohammad770@gmail.com>
2026-05-30 15:02:54 +0530

    feat:Using the ternary operator

--- src/com/codegnan/fundamentals/TernaryOperator.java
+++ src/com/codegnan/fundamentals/TernaryOperator.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 public class TernaryOperator {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int a = sc.nextInt();
+		System.out.println("Enter the number");
+		int b = sc.nextInt();
+		int GreatestNumber=(a>b)?a:b;
+		System.out.println("the greatest number is :"+GreatestNumber);
+		sc.close();
+		
+		
 		
 	}
 
